4c6c9fc98515be72f529598db5fb0e8fb1c3e03c
deaafebriann <deaafebriann@gmail.com>
2019-03-06 10:15:27 +0700

    laravel

---
+++ database/migrations/2019_03_06_013544_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('customer_name');
+            $table->string('identity_number');
+            $table->string('identity_photo');
+            $table->dateTime('return_date');
+            $table->float('price');
+            $table->enum('note', ['0','1']);
+            $table->date('added_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
